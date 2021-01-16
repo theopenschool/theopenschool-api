1a660832bfac054a59b2f1048e5f990ebd68951b
konstantinlois <konstantin.lois@gmx.de>
2021-01-16 18:57:31 +0100

    minor changes

--- src/main/java/com/openschool/api/controllers/TestController.java
+++ src/main/java/com/openschool/api/controllers/TestController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
-    // test route
+    
     @GetMapping("/test")
     public String test(){
         return "test";
