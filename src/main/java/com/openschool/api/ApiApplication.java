ee7053bbd80c2ae0bf2053c1f5ab1e58195415c3
konstantinlois <konstantin.lois@gmx.de>
2021-01-17 00:12:30 +0100

    minor changes

--- src/main/java/com/openschool/api/ApiApplication.java
+++ src/main/java/com/openschool/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.openschool.api;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -18,21 +19,6 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-
-		/*String hostname = System.getenv("RDS_HOSTNAME");
-		String port = System.getenv("RDS_PORT");
-		String dbname = System.getenv("RDS_DB_NAME");
-		String username = System.getenv("RDS_USERNAME");
-		String password = System.getenv("RDS_PASSWORD");
-
-		// Create the Flyway instance and point it to the database
-		Flyway flyway =
-				Flyway.configure()
-						.dataSource( "jdbc:postgresql://" + hostname + ":" + port + "/" + dbname, username , password )
-						.load();
-
-		// Start the migration
-		flyway.migrate();*/
 	}
 
 }
