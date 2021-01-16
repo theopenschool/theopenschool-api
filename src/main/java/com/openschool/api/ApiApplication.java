7e05a837a731480627979053adca5acdd58b23bc
konstantinlois <konstantin.lois@gmx.de>
2021-01-17 00:53:37 +0100

    added task resource

--- src/main/java/com/openschool/api/ApiApplication.java
+++ src/main/java/com/openschool/api/ApiApplication.java
@@ -3,8 +3,6 @@ package com.openschool.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.sql.DataSource;
-
 @SpringBootApplication
 public class ApiApplication {
 
